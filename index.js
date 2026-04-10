8b47a5647875cdb983c6b1ead030e30d3fe9ae2f
giaptl <156375308+giaptl@users.noreply.github.com>
2026-04-10 15:05:17 -0400

    fix mongoose ssl connection

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ import AssignmentsRoutes from "./kambaz/assignments/routes.js";
 
 
 const CONNECTION_STRING = process.env.DATABASE_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kambaz";
-mongoose.connect(CONNECTION_STRING);
+mongoose.connect(CONNECTION_STRING, {
+  tlsAllowInvalidCertificates: true,
+  serverSelectionTimeoutMS: 5000,
+});
 
 const app = express();
 
